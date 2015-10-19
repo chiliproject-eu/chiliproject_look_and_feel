65b4f0bd3946ab31b36d4924decf190fad5ac667
Jan Svoboda <jan@mluv.cz>
2015-10-20 01:54:07 +0200

    add jQuery UI theme to match chiliproject color scheme

--- lib/chiliproject_look_and_feel_hook_listener.rb
+++ lib/chiliproject_look_and_feel_hook_listener.rb
@@ -2,6 +2,7 @@ class ChiliprojectLookAndFeelHookListener < Redmine::Hook::ViewListener
 
   def view_layouts_base_html_head(context = {})
     stylesheet_link_tag('application', media: 'all', plugin: 'chiliproject_look_and_feel') +
+    stylesheet_link_tag('jquery/jquery-ui.theme', media: 'all', plugin: 'chiliproject_look_and_feel') +
     javascript_include_tag('application', media: 'all', plugin: 'chiliproject_look_and_feel')
   end
 
